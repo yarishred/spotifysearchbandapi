96cfeb7019e5390aa9973f886e773b46ce280287
Yari Ruiz <yfruiz9@gmail.com>
2021-01-23 08:16:44 -0500

    Update SpotifyCredentials.js

--- src/helpers/SpotifyCredentials.js
+++ src/helpers/SpotifyCredentials.js
@@ -1,7 +1,7 @@
 export const SpotifyCredentials = () => {
   return {
-    client_id: 'fcd512d1a95d4209876e98d9e7beffa0',
-    client_secret: 'c7fbb5e1edf8431a898c34f105771b27'
+    client_id: 'your client id',
+    client_secret: 'your secret'
   };
 };
 
